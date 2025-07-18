527d2aaae369165f7e034c83b721e96ed7d9e9e4
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-07-18 20:53:24 +0330

    feat: add initial configuration files for LazyVim setup

---
+++ lua/config/options.lua
@@ -0,0 +1,32 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
+
+-- vim.g.markdown_fenced_languages = {
+--   "html",
+--   "python",
+--   "ruby",
+--   "vim",
+--   "bash",
+--   "fish",
+--   "shell",
+--   -- Add more languages as needed
+-- }
+
+local opt = vim.opt
+
+-- Tabs & Indentation
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.tabstop = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
