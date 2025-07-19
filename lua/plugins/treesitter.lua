79b7d38f3ed071e76b293cd298946105b1870581
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-07-19 21:39:17 +0330

    feat(plugins): add treesitter plugin configuration

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        ensure_installed = { "fish" },
+        highlight = {
+            -- disable = { "markdown" },
+            -- additional_vim_regex_highlighting = false,
+        },
+    },
+}
