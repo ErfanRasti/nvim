002580bf556f35741ce26fac6a9b711c612ef1f2
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-07-22 07:54:54 +0330

    feat: close snacks before restoring the session
    To prevent the possible the possible errors discussed in
    https://github.com/rmagatti/auto-session/issues/454#event-18742274709 we
    should close `snacks.nvim` before restoring the session.

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -8,5 +8,13 @@ return {
     opts = {
         suppressed_dirs = { "~/", "~/projects", "~/Downloads", "/" },
         -- log_level = 'debug',
+        pre_restore_cmds = {
+            function()
+                local pickers = Snacks.picker.get({ source = "explorer" })
+                if pickers and pickers[1] then
+                    pickers[1]:close()
+                end
+            end,
+        },
     },
 }
