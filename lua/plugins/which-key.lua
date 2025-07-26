bb74443aa463f5fef910a17f9662c4d300ad6029
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-07-26 15:00:13 +0330

    refactor(which-key): simplify keybinding configurations

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,41 +1,30 @@
 return {
-    {
-        "folke/which-key.nvim",
-        opts = {
-            preset = "helix",
-        },
-        config = function(_, opts)
-            require("which-key").setup(opts)
-            require("which-key").add({
-                -- When remap = true, f another mapping exists for <Esc>, that mapping will be triggered when you press jjj.
-                --  If remap = false, Neovim will use the literal meaning of <Esc>, ignoring any other mappings for <Esc>.
-                {
-                    "jjj",
-                    "<Esc>",
-                    desc = "Back to normal mode",
-                    mode = "i",
-                    remap = true,
-                    icon = "󰌌",
-                },
-                {
-                    "<leader>gc",
-                    function()
-                        vim.cmd("AICommit")
-                    end,
-                    desc = "Commit using AI",
-                    mode = "n",
-                    remap = true,
-                },
-                {
-                    "<leader>at",
-                    function()
-                        vim.cmd("Copilot toggle")
-                    end,
-                    desc = "Toggle Copilot",
-                    mode = "n",
-                    remap = true,
-                },
-            })
-        end,
-    },
+  "folke/which-key.nvim",
+  opts = {
+    preset = "helix",
+  },
+  config = function(_, opts)
+    require("which-key").setup(opts)
+    require("which-key").add({
+      -- When remap = true, f another mapping exists for <Esc>, that mapping will be triggered when you press jjj.
+      --  If remap = false, Neovim will use the literal meaning of <Esc>, ignoring any other mappings for <Esc>.
+      -- <cmd> tells Neovim to execute the following command in command-line mode (like typing : in normal mode).
+      --  <cr> stands for "carriage return" (i.e., pressing Enter)
+      {
+        "jjj",
+        "<Esc>",
+        mode = "i",
+        desc = "Back to normal mode",
+        remap = true,
+        icon = "󰌌",
+      },
+            {
+        "<leader>at",
+        "<cmdCopilot toggle<cr>",
+        mode = "n",
+        desc = "Toggle Copilot",
+        remap = true,
+      },
+    })
+  end,
 }
