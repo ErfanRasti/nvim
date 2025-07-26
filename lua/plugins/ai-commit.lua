e92f6048c66e2b3e69bc1f5bf8d75a3396715b46
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-07-26 14:36:22 +0330

    feat(plugin): integrate AI-commit with which-key for better accessibility

--- lua/plugins/ai-commit.lua
+++ lua/plugins/ai-commit.lua
@@ -1,16 +1,25 @@
 return {
-    "vernette/ai-commit.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope.nvim",
-        "ellisonleao/dotenv.nvim", -- make sure dotenv is loaded
-    },
-    event = "VeryLazy",
-    config = function()
-        require("dotenv").setup()
-        vim.cmd("Dotenv ~/.config/nvim/.env") -- put your API key here as OPENROUTER_API_KEY="YOUR_KEY"
-        require("ai-commit").setup({
-            openrouter_api_key = vim.fn.getenv("OPENROUTER_API_KEY"),
-        })
-    end,
+  "vernette/ai-commit.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "ellisonleao/dotenv.nvim", -- make sure dotenv is loaded
+  },
+  event = "VeryLazy",
+  config = function()
+    require("dotenv").setup()
+    vim.cmd("Dotenv ~/.config/nvim/.env") -- put your API key here as OPENROUTER_API_KEY="YOUR_KEY"
+    require("ai-commit").setup({
+      openrouter_api_key = vim.fn.getenv("OPENROUTER_API_KEY"),
+    })
+    require("which-key").add({
+      {
+        "<leader>gc",
+        "<cmdAICommit<cr>",
+        mode = "n",
+        desc = "Commit using AI",
+        remap = true,
+      },
+    })
+  end,
 }
