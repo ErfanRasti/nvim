8b80538a1a7bc862f4ded2002866d8aed7ae7565
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-08-07 01:02:23 +0330

    feat(telescope): search hidden and gitignored files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,28 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      {
-        "<leader>ff",
-        function()
-          require("telescope.builtin").find_files({
-            no_ignore = true,
-            no_ignore_parent = true,
-            follow = true,
-            hidden = true,
-          })
-        end,
-        desc = "Find Files (including gitignored)",
-      },
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    {
+      "<leader>ff",
+      function()
+        require("telescope.builtin").find_files({
+          no_ignore = true,
+          no_ignore_parent = true,
+          follow = true,
+          hidden = true,
+        })
+      end,
+      desc = "Find Files (including gitignored)",
+    },
+    {
+      "<leader>sg",
+      function()
+        require("telescope.builtin").live_grep({
+          additional_args = function()
+            return { "--hidden", "--no-ignore", "--follow" } -- Flat list
+          end,
+        })
+      end,
+      desc = "Grep Text (all)",
     },
   },
 }
