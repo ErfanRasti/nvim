e92236573627e75dc8c64d69bcd9bc2a0bf6eb9a
Erfan Rasti <64990780+ErfanRasti@users.noreply.github.com>
2025-10-17 11:28:48 +0330

    feat(plugins): add mason packages

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,24 @@
+return {
+  "mason.nvim",
+  opts = {
+    ensure_installed = {
+      "bash-language-server bashls",
+      "hyprls",
+      "json-lsp jsonls",
+      "lua-language-server lua_ls",
+      "markdown-toc",
+      "markdownlint-cli2",
+      "marksman",
+      "prettier",
+      "pyright",
+      "ruff",
+      "shellcheck",
+      "shfmt",
+      "stylua",
+      "tailwindcss-language-server tailwindcss",
+      "taplo",
+      "tree-sitter-cli",
+      "vtsls",
+    },
+  },
+}
